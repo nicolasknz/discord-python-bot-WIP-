caa38582371641987237e92633b76684ce19d1b6
Arthur Freitas <arthurfscode@gmail.com>
2022-11-14 23:37:47 +0000

    :sparkles: first commit

--- core/handle_message.py
+++ core/handle_message.py
@@ -1,4 +1,11 @@
 from core import commands
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 
 def handle_message(msg):
@@ -7,4 +14,21 @@ def handle_message(msg):
     command_found['callback'](msg)
 
 
-handle_message('another')
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    command_found = [command for command in commands.value if command['key'] == message.content][0]
+
+    if command_found:
+        await message.channel.send(command_found['callback'](message))
+
+
+client.run('NDg2NjkzNTU5NjcyNzY2NDg1.G_unwN.OPe1Al06f7VW3csrZBWkd9_a39ysrSwobuSSV4')
+
+
